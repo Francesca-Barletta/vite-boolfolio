f858e11fc5575120874eb3ddde0016bfb03ff26f
Francesca Barletta <wallebar@gmail.com>
2024-06-12 18:32:00 +0200

    ADD: projects print in appVue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
 <AppHeader/>
-<AppMain/>
+<div>
+<h1>I miei Progetti:</h1>
+<div>
+    <ul>
+        <li v-for="project in projects" :key="project.id">
+            <h2>{{ project.progetto }}</h2>
+            <p>{{ project.descrizione }}</p>
+            <p>{{ project.link }}</p>
+        </li>
+    </ul>
+</div>
+    </div>
 <AppFooter/>
 </template>
 
 <script>
+import axios from 'axios'
 import AppHeader from './components/AppHeader.vue'
-import AppMain from './components/AppMain.vue'
 import AppFooter from './components/AppFooter.vue'
 
 
@@ -14,9 +25,26 @@ import AppFooter from './components/AppFooter.vue'
   export default {
     components: {
       AppHeader,
-      AppMain,
       AppFooter
-    }
+    },
+    data() {
+           return{
+            projects:[]
+           }
+        },
+        methods: {
+            fetchProjects() {
+                axios.get('http://127.0.0.1:8000/api/projects')
+                .then((res)=>{
+                    // console.log(res.data.results)
+                    this.projects = res.data.results
+                })
+            }
+        },
+        created(){
+            this.fetchProjects()
+        }
+     
   }
 </script>
 
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,15 @@
 <template>
     <div>
 <h1>I miei Progetti:</h1>
+<div>
+    <ul>
+        <li v-for="project in projects">
+            <h2>{{ project.progetto }}</h2>
+            <p>{{ project.descrizione }}</p>
+            <p>{{ project.link }}</p>
+        </li>
+    </ul>
+</div>
     </div>
 </template>
 
@@ -17,13 +26,14 @@ import axios from 'axios'
             fetchProjects() {
                 axios.get('http://127.0.0.1:8000/api/projects')
                 .then((res)=>{
-                    this.projects = res.data.results.data
+                    // console.log(res.data.results)
+                    this.projects = res.data.results
                 })
             }
         },
-        created(){
+        created() {
             this.fetchProjects()
-        }
+        },
      
     }
 </script>
