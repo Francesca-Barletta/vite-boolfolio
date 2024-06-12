775d0a308cf361a97d8e97e919c1bfd8e37b060c
Francesca Barletta <wallebar@gmail.com>
2024-06-12 17:53:21 +0200

    ADD: base components vue and imported in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
 <AppHeader/>
+<AppMain/>
+<AppFooter/>
 </template>
 
 <script>
 import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+import AppFooter from './components/AppFooter.vue'
+
+
+
   export default {
     components: {
-      AppHeader
+      AppHeader,
+      AppMain,
+      AppFooter
     }
   }
 </script>
